7a2c7fd31b4c3cfd84583709691b5a19017ba007
19Lida <lida19melnyk@gmail.com>
2024-07-05 12:57:07 +0300

    fixed 4.ts

--- src/generics/4.ts
+++ src/generics/4.ts
@@ -5,9 +5,7 @@ type User = {
   password: string;
 };
 
-function createOrUpdateUser(initialValues: User, updates: Partial<User>) {
-  return { ...initialValues, ...updates };
-}
+function createOrUpdateUser(initialValues: Partial<User>) {}
 
 createOrUpdateUser({
   email: "user@mail.com",
